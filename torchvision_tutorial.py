04ce43af314669debbd07c1f0de23a674efc0050
kur <rhdnfka94@gmail.com>
2024-02-01 16:55:10 +0900

    Improve output result

--- torchvision_tutorial.py
+++ torchvision_tutorial.py
@@ -477,7 +477,8 @@ lr_scheduler = torch.optim.lr_scheduler.StepLR(
 )
 
 # let's train it just for 2 epochs
-num_epochs = 2
+num_epochs = 10
+#num_epochs = 2
 
 for epoch in range(num_epochs):
     # train for one epoch, printing every 10 iterations
@@ -518,16 +519,19 @@ with torch.no_grad():
 image = (255.0 * (image - image.min()) / (image.max() - image.min())).to(torch.uint8)
 image = image[:3, ...]
 pred_labels = [f"pedestrian: {score:.3f}" for label, score in zip(pred["labels"], pred["scores"])]
+
 pred_boxes = pred["boxes"].long()
-output_image = draw_bounding_boxes(image, pred_boxes, pred_labels, colors="red")
+boxed = draw_bounding_boxes(image, pred_boxes, pred_labels)
 
 masks = (pred["masks"] > 0.7).squeeze(1)
-output_image = draw_segmentation_masks(output_image, masks, alpha=0.5, colors="blue")
+masked = draw_segmentation_masks(image, masks, alpha=0.5)
 
+from datetime import datetime
+timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
 
-plt.figure(figsize=(12, 12))
-plt.imshow(output_image.permute(1, 2, 0))
-plt.savefig("out/3.png")
+from torchvision.io import write_png
+#write_png(boxed, f'out/{timestamp}.boxes.png')
+write_png(masked, f'out/{timestamp}.masks.png')
 
 ######################################################################
 # The results look good!
